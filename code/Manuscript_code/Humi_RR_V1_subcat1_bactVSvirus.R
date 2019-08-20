4a7788e3651dc8be2210559696409313d2f3ae06
ryjohnson09 <johnsrc06@gmail.com>
2019-08-20 11:11:10 -0400

    Add PCA and RR for bacteria verus virus

---
+++ code/Manuscript_code/Humi_RR_V1_subcat1_bactVSvirus.R
@@ -0,0 +1,203 @@
+######################################################################
+# Name: Humi_RR_V1_subcat1_bactVSvirus.R
+# Author: Ryan Johnson
+# Date Created: 20 Aug 2019
+# Purpose: Functional probe RR to determine functional
+#  differences between bacterial vs viral infections @ visit 1
+######################################################################
+
+library(tidyverse)
+
+visits <- c(1)
+detection_method <- "culture"
+
+## Load data --------------------------------------------------------------------------------------------
+humichip <- suppressMessages(suppressWarnings(read_tsv("data/processed/Merged_humichip_Renormalized.tsv")))
+treat <- suppressMessages(suppressWarnings(read_csv("data/processed/TrEAT_Clinical_Metadata_tidy.csv")))
+ID_Decoder <- suppressMessages(suppressWarnings(read_csv("data/processed/ID_Decoder_Humichip.csv")))
+
+# Extract visit 1 samples (don't need to be matched with 4 or 5)
+ID_Decoder_select <- ID_Decoder %>%
+  filter(visit_number %in% visits)
+
+
+# Merge treat to ID_Decoder
+treat <- ID_Decoder_select %>% 
+  left_join(., treat, by = c("study_id" = "STUDY_ID"))
+
+
+## Filter treat to include treat samples of interest -----------------------------------------------
+
+treat_filter <- treat %>% 
+  # Remove LOP and PLA
+  filter(!Treatment %in% c("LOP", "PLA"))
+
+
+
+## Select patient ID's that contain pathogens of interest -----------------------------------------
+# Bacteria vs Viral
+bacteria_list <- c("ETEC|EAEC|EPEC|EHEC|EIEC|C_diff|STEC|Shigella|Salmonella|Vibrio|EcoliO157H7|Aeromonas|Yersinia|Campylobacter")
+
+
+treat_filter_bacVSvirus <- treat_filter %>% 
+  select(glomics_ID, study_id, ends_with(paste0("_", detection_method))) %>% 
+  mutate_at(vars(ends_with(paste0("_", detection_method))), list(~ifelse(. == "yes", 1, 0))) %>% 
+  # Remove parasites
+  select_at(vars(-matches("Giardia|E_histo|Crypto|Cyclospora"))) %>% 
+  # Identify bacteria versus virus patients
+  mutate(bacteria = rowSums(select_at(., vars(matches(bacteria_list))))) %>% 
+  mutate(virus = rowSums(select_at(., vars(contains("virus"))))) %>% 
+  # Select bactria only and virus only
+  filter(bacteria > 0 & virus == 0 | bacteria == 0 & virus > 0) %>% 
+  mutate(bact_or_virus = ifelse(bacteria > 0, "Bacteria", "Virus")) %>% 
+  select(glomics_ID, study_id, bact_or_virus)
+
+
+
+
+
+
+## Filter the humichip data to include only samples in treat_filter --------------------------------
+humichip_filtered <- humichip %>% 
+  select_at(c("Genbank.ID", "Gene", "Organism", "Lineage",
+              "Gene_category", "Subcategory1", "Subcategory2", treat_filter_bacVSvirus$glomics_ID))
+
+
+## Convert the Humichip data to relative abundance -------------------------------------------------
+humi_relabun <- humichip_filtered %>%
+  # only consider probes with a gene category designation (removes STR_SPE probes)
+  filter(!is.na(Gene_category)) %>%
+  # Convert all NA values to 0
+  mutate_at(vars(starts_with("X")), list(~ifelse(is.na(.), 0, .))) %>% 
+  # Convert all values to relative abundance
+  mutate_at(vars(starts_with("X")), list(~(./sum(.)) * 100))
+
+
+## Summarize Data ----------------------------------------------------------------------------------
+humi_grouped <- humi_relabun %>%
+  # Select specific category?
+  #filter(Subcategory1 == "ANTIBIOTIC_RESISTANCE") %>% 
+  # Select grouping column of interest and remove rest
+  select(Gene_category, Subcategory1, starts_with("X")) %>%
+  # Make long
+  gather(key = glomics_ID, value = rel_abun_value, -Subcategory1, -Gene_category) %>%
+  # Group by category of interest
+  group_by(glomics_ID, Gene_category, Subcategory1) %>%
+  # Calculate total relative abundance for each category
+  summarise(category_abundance = sum(rel_abun_value)) %>%
+  # Add in metadata
+  left_join(., treat_filter_bacVSvirus, by = "glomics_ID") %>% 
+  ungroup()
+
+
+## Calculate Response Ratio ------------------------------------------------------------------------
+
+humi_RR <- humi_grouped %>% 
+  # Group by Subcategory1
+  group_by(Gene_category, Subcategory1, bact_or_virus) %>% 
+  summarise(mean_signal = mean(category_abundance),
+            sd_signal = sd(category_abundance),
+            n = sum(!is.na(category_abundance))) %>% 
+  
+  # Spread the signal mean by Impact
+  ungroup() %>%
+  group_by(Gene_category, Subcategory1, bact_or_virus) %>%
+  spread(bact_or_virus, mean_signal) %>% 
+  
+  # Rename mean columns
+  rename(group1_mean = colnames(.)[length(colnames(.)) - 1],
+         group2_mean = colnames(.)[length(colnames(.))]) %>% 
+  
+  # Spread the sd and n columns by visit
+  mutate(sd_group1 = ifelse(!is.na(group1_mean), sd_signal, NA)) %>%
+  mutate(sd_group2 = ifelse(!is.na(group2_mean), sd_signal, NA)) %>%
+  mutate(n_group1 = ifelse(!is.na(group1_mean), n, NA)) %>%
+  mutate(n_group2 = ifelse(!is.na(group2_mean), n, NA)) %>%
+  select(-sd_signal, -n) %>% 
+  
+  # Compress NAs
+  ungroup() %>%
+  group_by(Gene_category, Subcategory1) %>%
+  summarise_all(list(~sum(., na.rm = T))) %>% 
+  
+  # Must have at least __ observations in each subcategory
+  filter(n_group1 >= 3) %>%
+  filter(n_group2 >= 3) %>%
+  
+  # Calculate SEM for each mean
+  mutate(SEM_group1 = sd_group1 / sqrt(n_group1)) %>%
+  mutate(SEM_group2 = sd_group2 / sqrt(n_group2)) %>%
+  
+  # Calculate the Response Ratio (RR)
+  mutate(RR = log(group2_mean / group1_mean)) %>%
+  
+  # Calculate the Standard error for the RR
+  mutate(SE_RR = sqrt((SEM_group1**2 / group1_mean**2) + (SEM_group2**2 / group2_mean**2))) %>%
+  
+  # Calcualte the 95% confidence interval for each RR
+  mutate(CI95 = abs(1.96 * SE_RR)) %>%
+  
+  # Add in keeper column if does not overlap 0
+  mutate(keeper = ifelse(0 > (RR - CI95) & 0 < (RR + CI95), "No", "Yes")) %>%
+  
+  # Make labels pretty
+  mutate(pretty_cat = str_to_title(Subcategory1)) %>%
+  mutate(pretty_genecat = str_to_title(Gene_category)) %>% 
+  mutate(pretty_cat = str_replace_all(pretty_cat, "_"," ")) %>%
+  mutate(pretty_genecat = str_replace_all(pretty_genecat, "_"," ")) %>%
+  
+  # Factor columns
+  #mutate(pretty_cat = fct_reorder(pretty_cat, RR)) %>%
+  ungroup()
+
+## Remove any cateogories where the mean was below threshold ----------------
+humi_RR_filtered <- humi_RR %>% 
+  filter(group1_mean > 0.01) %>% 
+  filter(group2_mean > 0.01) %>% 
+  # Remove any NA categories from subcats
+  filter(!is.na(Subcategory1)) %>% 
+  # Remove an non-significant categories?
+  filter(keeper == "Yes")
+
+
+## Plot ---------------------------------------------------------------------
+
+RR_plot <- ggplot(data = humi_RR_filtered) +
+  geom_hline(yintercept = 0, linetype = "dashed", size = 1) +
+  
+  # points and error bar
+  geom_point(aes(x = pretty_cat, y = RR), 
+             size = 3,
+             position = position_dodge(width = 0.4)) +
+  geom_errorbar(aes(ymin = RR - CI95, 
+                    ymax = RR + CI95, 
+                    x = pretty_cat),
+                width = 0.25, 
+                position = position_dodge(width = 0.4)) +
+  
+  
+  # plot labels
+  labs(y = "Response Ratio",
+       x = "") +
+  scale_x_discrete(position = "left") +
+  
+  theme_linedraw() +
+  coord_flip() +
+  facet_wrap(~pretty_genecat, ncol = 1, scales = "free_y") +
+  theme(
+    axis.title.x = element_text(size = 15),
+    axis.title.y = element_text(size = 15),
+    axis.text.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    plot.caption = element_text(hjust = 0.5),
+    strip.text = element_text(size = 12)
+  )
+
+RR_plot
+
+ggsave(plot = RR_plot, filename = "results/figures/Manuscript_Figures/RR_V1_subcat1_bactVSvirus.png",
+       height = 6,
+       width = 7)
+
---
+++ code/Manuscript_code/PCA_bacteriaVSvirus_V1.R
@@ -0,0 +1,217 @@
+##################################################################
+# Name: PCA_bacteriaVSvirus_V1.R
+# Author: Ryan Johnson
+# Date Created: 20 Aug 2019
+# Purpose: Functional probe PCA to determine functional
+#  differences between bacterial vs viral infections
+##################################################################
+
+library(tidyverse)
+library(vegan)
+
+probe_type <- "Functional"
+visits <- c(1)
+detection_method <- "culture"
+
+
+## Read in data -----------------------------------------------------------------------------------------
+humichip <- suppressWarnings(suppressMessages(read_tsv("data/processed/Merged_humichip_Renormalized.tsv")))
+treat <- suppressWarnings(suppressMessages(read_csv("data/processed/TrEAT_Clinical_Metadata_tidy.csv")))
+ID_Decoder <- suppressWarnings(suppressMessages(read_csv("data/processed/ID_Decoder_Humichip.csv")))
+
+# Extract visit 1 samples (don't need to be matched with 4 or 5)
+ID_Decoder_select <- ID_Decoder %>%
+  filter(visit_number %in% visits)
+
+
+# Merge treat to ID_Decoder
+treat <- ID_Decoder_select %>% 
+  left_join(., treat, by = c("study_id" = "STUDY_ID"))
+
+
+## Filter treat to include treat samples of interest -----------------------------------------------
+
+treat_filter <- treat %>% 
+  # Remove LOP and PLA
+  filter(!Treatment %in% c("LOP", "PLA"))
+
+
+
+## Select patient ID's that contain pathogens of interest -----------------------------------------
+# Bacteria vs Viral
+bacteria_list <- c("ETEC|EAEC|EPEC|EHEC|EIEC|C_diff|STEC|Shigella|Salmonella|Vibrio|EcoliO157H7|Aeromonas|Yersinia|Campylobacter")
+
+
+treat_filter_bacVSvirus <- treat_filter %>% 
+  select(glomics_ID, study_id, ends_with(paste0("_", detection_method))) %>% 
+  mutate_at(vars(ends_with(paste0("_", detection_method))), list(~ifelse(. == "yes", 1, 0))) %>% 
+  # Remove parasites
+  select_at(vars(-matches("Giardia|E_histo|Crypto|Cyclospora"))) %>% 
+  # Identify bacteria versus virus patients
+  mutate(bacteria = rowSums(select_at(., vars(matches(bacteria_list))))) %>% 
+  mutate(virus = rowSums(select_at(., vars(contains("virus"))))) %>% 
+  # Select bactria only and virus only
+  filter(bacteria > 0 & virus == 0 | bacteria == 0 & virus > 0) %>% 
+  mutate(bact_or_virus = ifelse(bacteria > 0, "Bacteria", "Virus")) %>% 
+  select(glomics_ID, study_id, bact_or_virus)
+
+
+
+
+
+
+## Filter the humichip data to include only samples in treat_filter --------------------------------
+humichip_filtered <- humichip %>% 
+  select_at(c("Genbank.ID", "Gene", "Organism", "Lineage",
+              "Gene_category", "Subcategory1", "Subcategory2", treat_filter_bacVSvirus$glomics_ID))
+
+
+
+
+## Probe Filtering ---------------------------------------------------------
+if(probe_type == "Functional"){
+  humi_probes_filtered <- humichip_filtered %>%
+    filter(Gene != "STR_SPE")
+} else if (probe_type == "Strain/Species"){
+  humi_probes_filtered <- humichip_filtered %>%
+    filter(Gene == "STR_SPE")
+} else if (probe_type == "All"){
+  humi_probes_filtered <- humichip_filtered
+} else {
+  stop("Error filter probes")
+}
+
+
+## Prepare Data for Ordination Analysis ----------------------------------------
+humi_matrix <- humi_probes_filtered %>% 
+  
+  # Set NA's to 0 and values not NA to original value
+  select(starts_with("X")) %>%
+  mutate_all(list(~ifelse(is.na(.), 0, .)))
+
+# Remove rows that equal 0
+humi_matrix <- humi_matrix[rowSums(humi_matrix) != 0,]
+
+# Return matrix
+humi_matrix <- as.matrix(humi_matrix)
+
+
+
+## Ordination Analysis ----------------------------------------------------------
+humi_PCA <- vegan::rda(t(humi_matrix))
+
+# Extract coordinates as tibble
+humi_coordinates <- as.data.frame(scores(humi_PCA, display = "sites")) %>%
+  rownames_to_column(var = "glomics_ID")
+
+
+
+# Get Proportion explained
+ord_prop_expl <- summary(eigenvals(humi_PCA))[2,] * 100
+
+
+
+## Merge Ordination Analysis with Metadata --------------------------------------
+humi_PCA_metadata <- humi_coordinates %>%
+  # Add study ID's
+  full_join(., treat_filter_bacVSvirus, by = "glomics_ID")
+
+
+
+## Plot ---------------------------------------------------------------------------------
+# Factor Columns
+
+
+# Aesthetic sizes
+axis_title_size <- 18
+axis_text_size <- 16
+title_size <- 20
+legend_text_size <- 13
+point_size <- 4
+
+
+# Set up base plot
+humi_PCA_base <- 
+  ggplot(humi_PCA_metadata) +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_text(size = axis_title_size),
+    axis.text.x = element_text(size = axis_text_size, hjust = 1),
+    axis.text.y = element_text(size = axis_text_size),
+    axis.title.y = element_text(size = axis_title_size),
+    plot.title = element_text(size = title_size, face = "bold"),
+    legend.text = element_text(size = legend_text_size),
+    legend.position = "bottom",
+    legend.title = element_blank()) +
+  guides(fill = guide_legend(override.aes = list(size=7)))
+
+# PCA Plot
+humi_PCA_plot <- humi_PCA_base +
+  xlab(paste0("PC1(", round(ord_prop_expl[[1]], 2), "%)")) +
+  ylab(paste0("PC2(", round(ord_prop_expl[[2]], 2), "%)")) +
+  geom_point(aes(x = PC1, 
+                 y = PC2, 
+                 color = bact_or_virus),
+             alpha = 1, size = point_size, fill = "black") +
+  stat_ellipse(aes(x = PC1, y = PC2, color = bact_or_virus), 
+               level = 0.75, size = 1.5, linetype = "dashed")
+
+humi_PCA_plot
+
+ggsave(plot = humi_PCA_plot, filename = "results/figures/Manuscript_Figures/PCA_bacteriaVSvirus_V1.png",
+       height = 7,
+       width = 8)
+
+
+## Perform Statistics ----------------------------------------------
+
+# Remove samples for which the groupings are NA values
+new_ord_meta <- humi_PCA_metadata %>%
+    filter(!is.na(bact_or_virus))
+
+new_matrix <- humi_matrix[ ,colnames(humi_matrix) %in% new_ord_meta$glomics_ID]
+
+
+# Display the groupings
+stats_groups <- paste("Groupings:", paste(unique(new_ord_meta$bact_or_virus), collapse = ", "))
+
+adonis_results <-
+  # Ensure that new_ord_meta$glomics_ID is in the
+  #  same order as colnames(new_matrix
+  if (!all(new_ord_meta$glomics_ID == colnames(new_matrix))){
+    stopApp("Error calculating adonis P-value")
+  } else {
+    
+    # Calculate adonis results
+    
+    adonis_temp <-
+      vegan::adonis(t(new_matrix) ~ new_ord_meta$bact_or_virus,
+                    method = "bray",
+                    perm = 99)
+    
+    paste("adonis p-value: ", adonis_temp$aov.tab$`Pr(>F)`[1])
+  }
+
+
+
+mrpp_results <-
+  # Ensure that new_ord_meta$glomics_ID is in the
+  #  same order as colnames(new_matrix())
+  if (!all(new_ord_meta$glomics_ID == colnames(new_matrix))){
+    stopApp("Error calculating mrpp P-value")
+  } else {
+
+    # Calculate mrpp results
+      mrpp_temp <-  vegan::mrpp(t(new_matrix),
+                                new_ord_meta$bact_or_virus)
+    
+    paste("mrpp p-value: ", mrpp_temp$Pvalue)
+  }
+
+
+## Print stats --------------------------------------------------
+stats_groups
+
+adonis_results
+
+mrpp_results

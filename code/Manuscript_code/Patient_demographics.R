16e5e37cf9ba0a35c34e76b3412f4d36763a6c2c
ryjohnson09 <johnsrc06@gmail.com>
2019-08-20 12:43:12 -0400

    Add patient demographics

---
+++ code/Manuscript_code/Patient_demographics.R
@@ -0,0 +1,9 @@
+##########################################
+# Title: Patient_Demographics.R
+# Author: Ryan Johnson
+# Date Created: 20 August 2019
+# Purpose: Calculate patient demographics
+##########################################
+
+library(tidyverse)
+library(gt)
